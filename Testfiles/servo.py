ec8484011769b0703b49c81726f45d45c2ed7eb6
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-11 14:25:48 +0100

    Suche nach 90°

--- Testfiles/servo.py
+++ Testfiles/servo.py
@@ -5,27 +5,14 @@ servoPIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 18 als PWM mit 50Hz
-p.start(2.5) # Initialisierung
+p = GPIO.PWM(servoPIN, 50)  # GPIO 18 als PWM mit 50Hz
+p.start(2.5)  # Initialisierung
 try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
-  
+    while True:
+        p.ChangeDutyCycle(11)
+        time.sleep(2)
+        p.ChangeDutyCycle(5)
+        time.sleep(2)
+    except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
