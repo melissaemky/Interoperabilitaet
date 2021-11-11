20b3f1f2b53039f8bfd91c8039c74b523d638753
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-11 14:38:23 +0100

    werte final

--- Testfiles/servo.py
+++ Testfiles/servo.py
@@ -9,9 +9,9 @@ p = GPIO.PWM(servoPIN, 50)  # GPIO 18 als PWM mit 50Hz
 p.start(2.5)  # Initialisierung
 try:
     while True:
-        p.ChangeDutyCycle(10.6)
+        p.ChangeDutyCycle(10)
         time.sleep(2)
-        p.ChangeDutyCycle(5.5)
+        p.ChangeDutyCycle(5)
         time.sleep(2)
 except KeyboardInterrupt:
     p.stop()
