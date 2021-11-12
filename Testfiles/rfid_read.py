e45ea1301a0691f432b43d93cb7eaa2f035dc2c7
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-12 12:03:16 +0100

    test

--- Testfiles/rfid_read.py
+++ Testfiles/rfid_read.py
@@ -5,8 +5,19 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
+TASTER = 20
+AUSGANG = 15
+
+GPIO.setup(TASTER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(AUSGANG, GPIO.OUT)
+
+while True:
+    tasterStatus = GPIO.input(TASTER)
+    if ((tasterStatus) == False):
+        print("Test")
+
 try:
-        id, text = reader.read()
-        print(id)
+    id, text = reader.read()
+    print(id)
 finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
