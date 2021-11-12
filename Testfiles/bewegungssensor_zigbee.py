b4b548a7f70622d953c778c1df5c6ae8085842ef
trimixtimo <timok95@gmail.com>
2021-11-12 15:50:19 +0100

    ..

--- Testfiles/bewegungssensor_zigbee.py
+++ Testfiles/bewegungssensor_zigbee.py
@@ -2,4 +2,4 @@ import requests
 import json
 pir =requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/00:50:43:c9:a3:39:4a:82-01-0500/')
 obj = json.loads(pir.text)
-print(obj["lastseen"])
+print(obj["state"])
