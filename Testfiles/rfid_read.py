3399547f4be22a7662e8823acf47ed0c9ba44780
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-12 12:24:58 +0100

    ...

--- Testfiles/rfid_read.py
+++ Testfiles/rfid_read.py
@@ -6,14 +6,9 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 GPIO.setup(21, GPIO.IN)
-GPIO.setup(16, GPIO.IN)
-while True:
-    if GPIO.input(21) == 1:
-        print("Bitte Karte zum Speichern auflegen!")
-    if GPIO.input(16) == 1:
-        print("Bitte Karte zum Löschen auflegen!")
+GPIO.input(21)
 
-try:
+'''try:
     id, text = reader.read()
     print(id)
 finally:
