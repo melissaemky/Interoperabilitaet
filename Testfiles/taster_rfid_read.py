dc665f44377b35d5ee2e0f007378c98cc2ae0907
trimixtimo <timok95@gmail.com>
2021-11-12 14:57:29 +0100

    GPIO Bezeichnung von BCM auf Board umgestellt

--- Testfiles/taster_rfid_read.py
+++ Testfiles/taster_rfid_read.py
@@ -4,9 +4,9 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-# GPIO.setmode(GPIO.BCM)
-GPIO.setup(29, GPIO.IN)  # Grüner Taster(Speichern)
-GPIO.setup(28, GPIO.IN)  # Blauer Taser(Speichern)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(38, GPIO.IN)  # Grüner Taster(Speichern)
+GPIO.setup(40, GPIO.IN)  # Blauer Taser(Speichern)
 
 l = []
 x = l.pop()
@@ -18,11 +18,11 @@ def speichern():
 
 
 while True:
-    if GPIO.input(21) == 0:
+    if GPIO.input(38) == 0:
         speichern()
         print(reader)
         time.sleep(2)
         print(x)
-    if GPIO.input(20) == 0:
+    if GPIO.input(40) == 0:
         print("Blauer Taster gedrückt")
         time.sleep(2)
