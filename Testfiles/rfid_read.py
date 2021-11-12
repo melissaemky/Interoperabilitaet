628642fac0de8b4f66ad9b778765c2f50585a811
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-12 12:27:28 +0100

    ..

--- Testfiles/rfid_read.py
+++ Testfiles/rfid_read.py
@@ -5,8 +5,25 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-GPIO.setup(21, GPIO.IN)
-GPIO.input(21)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.IN)
+
+for i in range(5):
+    GPIO.output(23, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(23, GPIO.LOW)
+    time.sleep(0.5)
+
+# Endlosschleife
+while True:
+    if GPIO.input(24) == 0:
+        # Ausschalten
+        GPIO.output(23, GPIO.LOW)
+    else:
+        # Einschalten
+        GPIO.output(23, GPIO.HIGH)
 
 '''try:
     id, text = reader.read()
