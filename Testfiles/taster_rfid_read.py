5409b8322e7b3fb36a9d48b399e488e98f0642c5
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-12 14:23:19 +0100

    Mit taster die rfid einlesen und speichern

---
+++ Testfiles/taster_rfid_read.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN)  # Grüner Taster(Speichern)
+GPIO.setup(20, GPIO.IN)  # Blauer Taser(Speichern)
+
+l = []
+
+
+def speichern():
+    id, text = reader.read()
+    l.append(id)
+
+
+while True:
+    if GPIO.input(21) == 0:
+        speichern()
+        print(reader)
+        time.sleep(2)
+    if GPIO.input(20) == 0:
+        print("Blauer Taster gedrückt")
+        time.sleep(2)
