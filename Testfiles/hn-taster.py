042f6dc77abe31add2bff2230b43de80111120d3
alex060202 <al3x060202@gmail.com>
2021-11-12 15:55:00 +0100

    ..

--- Testfiles/hn-taster.py
+++ Testfiles/hn-taster.py
@@ -1,18 +1,20 @@
 import RPi.GPIO as GPIO
 import time
-import pywhatkit as kit
+from twilio.rest import Client
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
-
-
+ACCOUNT_SID = 'AC3b9a640bf840e9f26596a841e249f686'
+AUTH_TOKEN = 'fdf9ae04ae3555c0dbd08aed5e1eec31'
+FROM = 'whatsapp:+14155238886'
+To = 'whatsapp:+4915770217327'
+whatsapp= 
 # Endlosschleife
 while True:
     if GPIO.input(16) == 0:
         print("!!!Hausnotrum ALARM!!!")
-        kit.sendwhatmsg_instantly(f"+4915783867643","!!!Hausnotrum ALARM!!!")
-        #kit.sendwhats_image(phone_no="+4915783867643",img_path="img/Oma.jpg",caption="!!!Alarm!!! Oma braucht Hilfe")
-        #Tür geht auf
+        client = Client(ACCOUNT_SID, AUTH_TOKEN)
+        client.message.create(body=whatsapp, from_=FROM, to=To)
         time.sleep(300)
 
         
