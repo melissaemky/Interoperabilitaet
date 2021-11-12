c2f3c9ba32fc418bf5fb4f7789a4706e5babf431
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-12 13:39:32 +0100

    Taster testen

--- Testfiles/taster.py
+++ Testfiles/taster.py
@@ -3,6 +3,12 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN)
 
+if GPIO.input(21) == 0:
+    print("Taster nicht gedrückt")
+else:
+    print("Taster gedrückt")
+
+'''
 # Endlosschleife
 while True:
     if GPIO.input(21) == 0:
@@ -10,3 +16,4 @@ while True:
     # Ausschalten
     else:
         # Einschalten
+'''
