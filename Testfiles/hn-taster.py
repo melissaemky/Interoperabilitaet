83fff0563eb3f1790469f9b48fcf52130041ae89
alex060202 <al3x060202@gmail.com>
2021-11-13 17:41:35 +0100

    Whatsapp senden solte gehen

--- Testfiles/hn-taster.py
+++ Testfiles/hn-taster.py
@@ -4,11 +4,13 @@ from twilio.rest import Client
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
+
 ACCOUNT_SID = 'AC3b9a640bf840e9f26596a841e249f686'
 AUTH_TOKEN = 'fdf9ae04ae3555c0dbd08aed5e1eec31'
 FROM = 'whatsapp:+14155238886'
 To = 'whatsapp:+4915770217327'
-whatsapp= 
+whatsapp= f'!!!Hausnotrum ALARM!!!'
+
 # Endlosschleife
 while True:
     if GPIO.input(16) == 0:
@@ -16,5 +18,3 @@ while True:
         client = Client(ACCOUNT_SID, AUTH_TOKEN)
         client.message.create(body=whatsapp, from_=FROM, to=To)
         time.sleep(300)
-
-        
