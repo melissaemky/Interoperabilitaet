688d202573c7a747cfb9df58857824ebde011f7e
alex060202 <al3x060202@gmail.com>
2021-11-13 20:11:56 +0100

    Tagestaste angefangen

--- Testfiles/Hausnotruf/hn-tagestaste.py
+++ Testfiles/Hausnotruf/hn-tagestaste.py
@@ -1,9 +1,13 @@
 import ntplib
 from time import ctime
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(XXX, GPIO.IN) ###Button GPIO pin setzen
 
 ###pip install ntplib
 
-
+#Zeit aus Netz holen
 def print_time():
     ntpClient = ntplib.ntpClient()
     response = ntpClient.request('pool.ntp.org')
@@ -12,3 +16,9 @@ def print_time():
 
 if __name__ == "__main__":
         print_time()
+
+#Letzten Buttondruck abfragen
+while True:
+    if GPIO.input(XXX) == 0:
+        #die letzte Tagestasten-Zeit mit der aktuellen Zeit vergleichen, wenn Stunden Unterschied > 12h = Alarm
+        time.sleep(300)
