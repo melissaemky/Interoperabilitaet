287f3e8fe6c975a2a79a16af4498887ac2b1d823
alex060202 <al3x060202@gmail.com>
2021-11-15 12:42:48 +0100

    datetime test

--- testfiles/Hausnotruf/hn-taster.py
+++ testfiles/Hausnotruf/hn-taster.py
@@ -1,33 +1,27 @@
 import RPi.GPIO as GPIO
 from twilio.rest import Client
 import time
-#import ntplib
-#from time import ctime
+from datetime import datetime
 
+#Timestamp
+x = datetime.now()
+
+#GPIO set
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
+
 # Einrichtung SMS
 account_sid = "AC3b9a640bf840e9f26596a841e249f686"
 auth_token = "434dd84cb2c80899756e26bcfac488e5"
 FROM= '+12674592798'
 TO= '+4915770217327'
 
-# Zeitstempel
-"""def print_time():
-    ntpClient = ntplib.ntpClient()
-    response = ntpClient.request('pool.ntp.org')
-
-    print(ctime(response.tx_time))
-
-if __name__ == "__main__":
-        print_time()"""
-
-# Endlosschleife
+# Endlosschleife Button press
 while True:
     if GPIO.input(16) == 0:
-        print("!!!Hausnotruf ALARM!!!")
+        print("!!!Hausnotruf ALARM!!! \n {x}")
         Client(account_sid, auth_token).messages.create(
-            body= "!!!Hausnotruf ALARM!!!",
+            body= "!!!Hausnotruf ALARM!!! \n {x}",
             to= TO,
             from_= FROM)
         time.sleep(10)
