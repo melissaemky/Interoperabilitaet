d022e60784f97e931e05843c290ef3d491c9c530
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-15 11:13:10 +0100

    configparser erster commit

--- Testfiles/taster_rfid_read.py
+++ Testfiles/taster_rfid_read.py
@@ -4,6 +4,7 @@ from mfrc522 import SimpleMFRC522
 import configparser
 
 reader = SimpleMFRC522()
+cfg = configparser.ConfigParser()
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38, GPIO.IN)  # Blauer Taster(Speichern)
@@ -12,16 +13,15 @@ GPIO.setup(40, GPIO.IN)  # Grüner Taser(Speichern)
 l = []
 id = ''
 
-logfile = open('/home/pi/Heizungssteuerung/logfile.txt','a')
 
-cfg = configparser.ConfigParser()
 cfg.read('/home/pi/Heizungssteuerung/heizung-config.ini')
 
 heizdauer = cfg.get('Parameter', 'Heizdauer')
 
-cfgfile = open("/home/pi/Heizungssteuerung/heizung-config.ini",'w')
-cfg.set('DoNotChange','heizen',str(heizung_status))
-cfg.set('DoNotChange','letzteheizperiode', letzte_heizung.strftime("%d/%m/%Y, %H:%M:%S")) 
+cfgfile = open("/home/pi/Heizungssteuerung/heizung-config.ini", 'w')
+cfg.set('DoNotChange', 'heizen', str(heizung_status))
+cfg.set('DoNotChange', 'letzteheizperiode',
+        letzte_heizung.strftime("%d/%m/%Y, %H:%M:%S"))
 cfg.write(cfgfile)
 cfgfile.close()
 
