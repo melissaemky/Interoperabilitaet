77548b2b6b195ce648ee32f89143f79dce97fb8b
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-15 11:13:54 +0100

    configparser

---
+++ Testfiles/configparser_test.py
@@ -0,0 +1,12 @@
+import configparser
+cfg = configparser.ConfigParser()
+
+cfg.read('/home/pi/interoperabilitaet/config_dateien/benutzer.ini')
+
+#benutzer = cfg.get('', 'Heizdauer')
+
+cfgfile = open("/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
+cfg.set('benutzer1', 'id', 'test')
+cfg.set('benutzer1', 'zugang', 'darf rein')
+cfg.write(cfgfile)
+cfgfile.close()
