f2bd3b71d0239c5ec3c3f025c52b58658af62d8f
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-15 11:43:26 +0100

    configparser eingebaut

--- testfiles/configparser_test.py
+++ testfiles/configparser_test.py
@@ -4,10 +4,10 @@ cfg = configparser.ConfigParser()
 cfg.read('/home/pi/interoperabilitaet/config_dateien/benutzer.ini')
 
 benutzer = cfg.get('benutzer1', 'zugang')
-print(benutzer)
-'''cfgfile = open("/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
-cfg.add_section('benutzer1')
+
+cfgfile = open("/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
+cfg.add_section(id)
 cfg.set('benutzer1', 'id', 'test')
 cfg.set('benutzer1', 'zugang', 'darf rein')
 cfg.write(cfgfile)
-cfgfile.close()'''
+cfgfile.close()
