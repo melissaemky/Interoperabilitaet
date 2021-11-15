7997bce4fa0fdd9480e1c1cb402161b05ee70e78
alex060202 <al3x060202@gmail.com>
2021-11-15 10:01:25 +0100

    ..

--- Testfiles/Hausnotruf/hn-taster.py
+++ Testfiles/Hausnotruf/hn-taster.py
@@ -9,12 +9,11 @@ ACCOUNT_SID = 'AC3b9a640bf840e9f26596a841e249f686'
 AUTH_TOKEN = 'fdf9ae04ae3555c0dbd08aed5e1eec31'
 FROM = 'whatsapp:+14155238886'
 To = 'whatsapp:+4915770217327'
-whatsapp= f'!!!Hausnotrum ALARM!!!'
 
 # Endlosschleife
 while True:
     if GPIO.input(16) == 0:
         print("!!!Hausnotrum ALARM!!!")
         client = Client(ACCOUNT_SID, AUTH_TOKEN)
-        client.message.create(body=whatsapp, from_=FROM, to=To)
+        client.message.create(body='!!!Hausnotrum ALARM!!!', from_=FROM, to=To)
         time.sleep(300)
