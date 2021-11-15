b62982b947785da92c0e854db6003bcd3372b176
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-15 10:30:49 +0100

    löschen funktion

--- Testfiles/taster_rfid_read.py
+++ Testfiles/taster_rfid_read.py
@@ -9,25 +9,30 @@ GPIO.setup(38, GPIO.IN)  # Blauer Taster(Speichern)
 GPIO.setup(40, GPIO.IN)  # Grüner Taser(Speichern)
 
 l = []
+id = ''
 
 
-# def speichern():
-id, text = reader.read()
-l.append(id)
+def speichern():
+    id, text = reader.read()
+    l.append(id)
+
+
+def löschen():
+    id, text = reader.read()
+    l.remove(id)
 
 
 while True:
     if GPIO.input(40) == 0:
         time.sleep(5)
-        # speichern()
-
-        def speichern():
-            id, text = reader.read()
-            l.append(id)
+        speichern()
         time.sleep(2)
         x = l.pop()
         print(id)
         print(x)
     if GPIO.input(38) == 0:
-        print("Blauer Taster gedrückt")
+        time.sleep(5)
+        löschen()
         time.sleep(2)
+        print(id)
+        print(l)
