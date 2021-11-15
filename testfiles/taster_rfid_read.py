00f332ec8467216077e2084fbfe946d9ca4e6cc4
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-15 12:38:29 +0100

    configparser verbessert

--- testfiles/taster_rfid_read.py
+++ testfiles/taster_rfid_read.py
@@ -10,34 +10,42 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38, GPIO.IN)  # Blauer Taster(Speichern)
 GPIO.setup(40, GPIO.IN)  # Grüner Taser(Speichern)
 
-id = ''
-cfg.read('/home/pi/interoperabilitaet/config_dateien/benutzer.ini')
-
 
 def speichern():
     id, text = reader.read()
-    cfgfile = open(
-        "/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
-    cfg.add_section(str(id))
-    cfg.write(cfgfile)
-    cfgfile.close()
+    if cfg.has_section(id):
+        cfgfile = open(
+            "/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
+        cfg.set(str(id), 'zugang', 'gestattet')
+        cfg.write(cfgfile)
+        cfgfile.close()
+        print(id + " Zugang gestattet")
+    else:
+        cfgfile = open(
+            "/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
+        cfg.add_section(str(id))
+        cfg.set(str(id), 'zugang', 'gestattet')
+        cfg.write(cfgfile)
+        cfgfile.close()
+        print(id + " gespeichert und Zugang gestattet")
 
 
 def löschen():
     id, text = reader.read()
-    cfgfile = open(
-        "/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
-    cfg.remove_section(str(id))
-    cfg.write(cfgfile)
-    cfgfile.close()
+    if cfg.has_section(id):
+        cfgfile = open(
+            "/home/pi/interoperabilitaet/config_dateien/benutzer.ini", 'w')
+        cfg.set(str(id), 'zugang', 'verweigert')
+        cfg.write(cfgfile)
+        cfgfile.close()
+        print(id + " Zugang verweigert")
 
 
 while True:
+    cfg.read('/home/pi/interoperabilitaet/config_dateien/benutzer.ini')
     if GPIO.input(40) == 0:
         time.sleep(5)
         speichern()
-        print(id)
     if GPIO.input(38) == 0:
         time.sleep(5)
         löschen()
-        print(id)
