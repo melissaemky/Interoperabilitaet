22f8f8610fc743e4b5e4f05e8d06a8be6142361a
trimixtimo <timok95@gmail.com>
2021-11-15 10:36:26 +0100

    ..

--- Testfiles/bewegungssensor_zigbee.py
+++ Testfiles/bewegungssensor_zigbee.py
@@ -4,6 +4,7 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
+raum = 0
 
 while raum == 0:
     pir =requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/00:50:43:c9:a3:39:4a:82-01-0500/')
