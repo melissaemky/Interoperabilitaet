e54d9275c0b0623c80b4165900c945733b255c0d
alex060202 <al3x060202@gmail.com>
2021-11-15 10:53:07 +0100

    ..

--- Testfiles/Hausnotruf/hn-taster.py
+++ Testfiles/Hausnotruf/hn-taster.py
@@ -1,21 +1,21 @@
 import RPi.GPIO as GPIO
-import os
-from  twilio import Client
+import twilio
+import twilio.rest
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
 
-ACCOUNT_SID = 'AC3b9a640bf840e9f26596a841e249f686'
-AUTH_TOKEN = 'fdf9ae04ae3555c0dbd08aed5e1eec31'
-FROM = '+14155238886'
-TO = '+4915770217327'
+account_sid = 'AC3b9a640bf840e9f26596a841e249f686'
+auth_token = 'fdf9ae04ae3555c0dbd08aed5e1eec31'
+FROM= '+14155238886'
+TO= '+4915770217327'
 
 # Endlosschleife
 while True:
     if GPIO.input(16) == 0:
         print("!!!Hausnotrum ALARM!!!")
-        client = Client(ACCOUNT_SID, AUTH_TOKEN)
-        client.message.create(
+        client = twilio.rest.TwilioRestClient(account_sid, auth_token)
+        client.messages.create(
             to=TO,
             from_=FROM,
             body="!!!Hausnotrum ALARM!!!")
