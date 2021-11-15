6c983f8918cb1e78b4cc1540e22f5d808fbbae1b
alex060202 <al3x060202@gmail.com>
2021-11-15 11:38:12 +0100

    ..

--- testfiles/Hausnotruf/hn-taster.py
+++ testfiles/Hausnotruf/hn-taster.py
@@ -5,8 +5,8 @@ import twilio.rest
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
 
-account_sid = 'AC3b9a640bf840e9f26596a841e249f686'
-auth_token = 'fdf9ae04ae3555c0dbd08aed5e1eec31'
+account_sid = "AC3b9a640bf840e9f26596a841e249f686"
+auth_token = "fdf9ae04ae3555c0dbd08aed5e1eec31"
 FROM= '+14155238886'
 TO= '+4915770217327'
 
