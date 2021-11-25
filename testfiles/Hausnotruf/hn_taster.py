3a8966b2ca04fd365845dd172f19e366303f15de
alex060202 <al3x060202@gmail.com>
2021-11-25 11:53:40 +0100

    ..

--- testfiles/Hausnotruf/hn_taster.py
+++ testfiles/Hausnotruf/hn_taster.py
@@ -6,7 +6,7 @@ import hn_tagestaste
 import configparser
 
 import sys                                                          #Twilio Anmeldedaten einbinden
-sys.path.insert(1,"/home/pi")
+sys.path.insert(1,"/home/pi/config_dateien")
 from twilio_anmeldung import auth_token, account_sid, FROM, TO
 
 cfg = configparser.ConfigParser()
