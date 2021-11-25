066c9c7e4f6ebbd85218629ece530dd86d9b0922
alex060202 <al3x060202@gmail.com>
2021-11-25 11:03:41 +0100

    ..

--- testfiles/Hausnotruf/hn_taster.py
+++ testfiles/Hausnotruf/hn_taster.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 from twilio.rest import Client
 import time
 from datetime import datetime
-import hn_tagestaste as tagestaste
+import hn_tagestaste
 
 #Timestamp
 x = datetime.now()
@@ -13,8 +13,8 @@ GPIO.setup(16, GPIO.IN)
 
 #hn-tagestaste - countdown restart
 if GPIO.input(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27) == 0:
-    tagestaste.countdown.exit()
-    tagestaste.countdown(int(t))
+    hn_tagestaste.countdown.exit()
+    hn_tagestaste.countdown(int(t))
 
 # Einrichtung SMS
 account_sid = "AC3b9a640bf840e9f26596a841e249f686"
