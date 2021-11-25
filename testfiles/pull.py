d70b0de51c767c9bb77687d35861ae91a11efae9
trimixtimo <timok95@gmail.com>
2021-11-25 14:54:59 +0100

    pin von bcm auf board geändert

--- testfiles/pull.py
+++ testfiles/pull.py
@@ -3,9 +3,9 @@ import os
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7, GPIO.IN)  # Grüner Taster(Speichern)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(26, GPIO.IN)  # Grüner Taster(Speichern)
 
 while True:
-    if GPIO.input(7) == 0:
+    if GPIO.input(26) == 0:
         os.system("git pull")
