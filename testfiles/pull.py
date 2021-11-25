399495a579a32facb9e84a8fbb321db6e4207383
trimixtimo <timok95@gmail.com>
2021-11-25 11:08:23 +0100

    pull mit taster

--- testfiles/pull.py
+++ testfiles/pull.py
@@ -1,2 +1,10 @@
 import os
-os.system("git pull")
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(7, GPIO.IN)  # Grüner Taster(Speichern)
+
+while True:
+    if GPIO.input(7) == 0:
+        os.system("git pull")
