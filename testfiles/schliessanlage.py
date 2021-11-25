b1371487a85add1155abdf509c92ec92ea4e6b0d
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-25 13:28:38 +0100

    schliessanlage-datei erstellt

---
+++ testfiles/schliessanlage.py
@@ -0,0 +1,2 @@
+# Wenn karte vor tür (RFID-Sensor) gehalten wird und Karte in benutzer.ini als zugelassen gilt, dann fürhre funktion tuerauf() aus
+# Endlosschleife durch drücken der beiden vorhandenen Knöpfe für gewisse Zeit unterbrechen
