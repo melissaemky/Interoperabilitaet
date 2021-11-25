38bb866bdefebd6599ba94373ee99a34964149f9
alex060202 <al3x060202@gmail.com>
2021-11-25 11:45:39 +0100

    ..

--- testfiles/Hausnotruf/hn_tagestaste.py
+++ testfiles/Hausnotruf/hn_tagestaste.py
@@ -7,7 +7,7 @@ def countdown(t):                                       #13h countdown
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        print(timer)
         time.sleep(1)
         t -= 1
         hn_taster.alarm()                               #Alarmfunktion aus hn_taster ausführen
