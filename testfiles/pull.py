54dd60d1e30f47558e0c3cff010f3ca97dc012f7
trimixtimo <timok95@gmail.com>
2021-11-29 10:32:38 +0100

    .

--- testfiles/pull.py
+++ testfiles/pull.py
@@ -9,8 +9,5 @@ GPIO.setup(26, GPIO.IN)
 while True:
     if GPIO.input(26) == 0:
         os.system("git pull")
-<<<<<<< HEAD
 
         test
-=======
->>>>>>> e6fb9c0250d26c5b78f04f2540258da5cbe10762
