ab0e91152f11f90b0ae2bd3be2f8ebc2d574ee89
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-11-29 15:03:23 +0100

    ...

--- testfiles/testfiles_xml/xml_test.py
+++ testfiles/testfiles_xml/xml_test.py
@@ -3,6 +3,11 @@ xml_tree = ET.parse('/home/pi/config_dateien/univers.xml')
 xml_root = xml_tree.getroot()
 # Header
 print('Tutorial List :')
+for movie in xml_root.iter('Tutorial'):
+    print(movie.attrib)
+
+'''
 for xml_elmt in xml_root:
     for inner_elmt in xml_elmt:
         print(inner_elmt.text)
+'''
