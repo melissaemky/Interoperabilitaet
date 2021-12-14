2f7041b68d94ef3d915954f167692fa0b8c2458d
trimixtimo <timok95@gmail.com>
2021-12-14 16:58:14 +0100

    .

--- testfiles/humidity.py
+++ testfiles/humidity.py
@@ -97,16 +97,16 @@ class CurrentValues():
         return cls(round(data[1]/100.0 - 273.15, 2), data[3]/100.0, data[4])
 
     def __str__(self):
-        #msg = 'Temperature: {}, '.format(self.temperature)
-        #msg += '"Humidity": "{}", '.format(self.humidity)
-        #msg += '"VOC": "{}"'.format(self.voc)
+        msg = 'Temperature: {}, '.format(self.temperature)
+        msg += '"Humidity": "{}", '.format(self.humidity)
+        msg += '"VOC": "{}"'.format(self.voc)
         
         with open ("/home/pi/config_dateien/universe.json") as json_file:
             x = json.load(json_file)
         x["sensoren"][0]["messwert"] = self.temperature
         with open("/home/pi/config_dateien/universe.json", 'w') as json_file:
             json.dump(x, json_file, indent=4)
-
+        print(msg)
         #return msg
 
 
