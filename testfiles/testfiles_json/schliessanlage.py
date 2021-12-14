2e7c4075150771fc40030987e75d77ffe4cdf144
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-12-14 14:49:24 +0100

    ...

---
+++ testfiles/testfiles_json/schliessanlage.py
@@ -0,0 +1,27 @@
+# Wenn karte vor tür (RFID-Sensor) gehalten wird und Karte in benutzer.ini als zugelassen gilt, dann fürhre funktion tuerauf() aus
+# Endlosschleife durch drücken der beiden vorhandenen Knöpfe für gewisse Zeit unterbrechen
+import time
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+from servo import tuerauf, tuerzu
+import configparser
+cfg = configparser.ConfigParser()
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(38, GPIO.IN)  # Blauer Taster(Speichern)
+GPIO.setup(40, GPIO.IN)  # Grüner Taser(Löschen)
+
+reader = SimpleMFRC522()
+
+while True:
+    if GPIO.input(38) == 0 or GPIO.input(40) == 0:
+        time.sleep(30)
+    else:
+        id, text = reader.read()
+        print(id)
+        cfg.read('/home/pi/config_dateien/benutzer.ini')
+        if cfg.has_section(str(id)) == True:
+            tuerauf()
+        else:
+            tuerzu()
+    GPIO.cleanup()
