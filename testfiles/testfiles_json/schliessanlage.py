b4a118952b3f85325e16d9a7cff562802da28f31
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-12-14 17:52:21 +0100

    ...

--- testfiles/testfiles_json/schliessanlage.py
+++ testfiles/testfiles_json/schliessanlage.py
@@ -4,8 +4,8 @@ import time
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from servo import tuerauf, tuerzu
-import configparser
-cfg = configparser.ConfigParser()
+import json
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38, GPIO.IN)  # Blauer Taster(Speichern)
@@ -15,13 +15,51 @@ reader = SimpleMFRC522()
 
 while True:
     if GPIO.input(38) == 0 or GPIO.input(40) == 0:
-        time.sleep(30)
+        time.sleep(30)  # Hier fehlt noch speichern und löschen
     else:
         id, text = reader.read()
         print(id)
-        cfg.read('/home/pi/config_dateien/benutzer.ini')
-        if cfg.has_section(str(id)) == True:
-            tuerauf()
-        else:
-            tuerzu()
+
+        with open('/home/pi/config_dateien/universetest.json') as json_file:
+            x = json.load(json_file)
+            lk = len(x['karten'])
+            lb = len(x['benutzer'])
+            la = len(x['aktoren'])
+            for i in range(0, lk):
+                kartennummer = (x['karten'][i]['kartennummer'])
+                if kartennummer == id:
+                    kartenid = (x['karten'][i]['id'])
+                    print("Kartennummer gefunden")
+                    for j in range(0, lb):
+                        benutzerkarte = (x['benutzer'][j]['kartenid'])
+                        if kartenid == benutzerkarte:
+                            zugang = (x['benutzer'][j]['zugang'])
+                            print("Benutzer gefunden")
+                            if zugang == "ja":
+                                print("Zugang: ja")
+                                for k in range(0, la):
+                                    aktor = (x['aktoren'][k]['typ'])
+                                    if aktor == "haustür":
+                                        print("Haustür gefunden")
+                                        zustand = (x['aktoren'][k]['zustand'])
+                                        if zustand == "0":
+                                            print("Tür war zu, wird geöffnet")
+                                            tuerauf()
+                                            # Zustand "Türauf"
+                                            (x['aktoren'][k]['zustand']) = "1"
+                                            # Neuen Zustand speichern
+                                            with open('/home/pi/config_dateien/universetest.json', 'w') as json_file:
+                                                json.dump(
+                                                    x, json_file, indent=4)
+                                        else:
+                                            print(
+                                                "Tür war auf, wird geschlossen")
+                                            tuerzu()
+                                            # Zustand "Türzu"
+                                            (x['aktoren'][k]['zustand']) = "0"
+                                            # Neuen Zustand speichern
+                                            with open('/home/pi/config_dateien/universetest.json', 'w') as json_file:
+                                                json.dump(
+                                                    x, json_file, indent=4)
+
     GPIO.cleanup()
