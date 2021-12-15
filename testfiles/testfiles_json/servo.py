0aaeb933d88af06efb4e7386f01c4c5a30de1b7d
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-12-15 13:02:46 +0100

    uni 1 entfernt

--- testfiles/testfiles_json/servo.py
+++ testfiles/testfiles_json/servo.py
@@ -1,25 +1,22 @@
 import time
-uni = 1
-if uni == 1:
-    import RPi.GPIO as GPIO
-    servoPIN = 12
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(servoPIN, GPIO.OUT)
-    GPIO.setwarnings(False)
-    p = GPIO.PWM(servoPIN, 50)  # GPIO 12 als PWM mit 50Hz
-    p.start(10)  # Initialisierung
+
+import RPi.GPIO as GPIO
+servoPIN = 12
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setwarnings(False)
+p = GPIO.PWM(servoPIN, 50)  # GPIO 12 als PWM mit 50Hz
+p.start(10)  # Initialisierung
 
 
 def tuerzu():
-    if uni == 1:
-        p.ChangeDutyCycle(10)
+    p.ChangeDutyCycle(10)
     print("Tür ist geschlossen!")
     time.sleep(2)
 
 
 def tuerauf():
-    if uni == 1:
-        p.ChangeDutyCycle(3.5)
+    p.ChangeDutyCycle(3.5)
     print("Tür ist geöffnet!")
     time.sleep(2)
 
