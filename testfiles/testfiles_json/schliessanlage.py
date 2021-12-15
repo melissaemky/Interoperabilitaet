410228cc8114c6bb3a30a2200d02ef8201464c6e
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-12-15 11:27:23 +0100

    .

--- testfiles/testfiles_json/schliessanlage.py
+++ testfiles/testfiles_json/schliessanlage.py
@@ -18,7 +18,7 @@ while True:
         time.sleep(30)  # Hier fehlt noch speichern und löschen
     else:
         id, text = reader.read()
-        print(id)
+        print("Karte gelesen" + id)
 
         with open("/home/pi/config_dateien/universetest.json") as json_file:
             x = json.load(json_file)
@@ -29,7 +29,7 @@ while True:
         for i in range(0, lk):
             print("in der for schleife")  # kann später weg
             kartennummer = (x['karten'][i]['kartennummer'])
-            print(kartennummer)
+            print(i + "te Kartennummer" + kartennummer)  # kann später weg
             if kartennummer == id:
                 print("in der if abfrage")  # kann später weg
                 kartenid = (x['karten'][i]['id'])
