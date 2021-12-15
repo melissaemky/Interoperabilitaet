77f9d3dece8d0e82330c9ff250d9cbaf6bc41f8f
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-12-15 13:01:39 +0100

    uni = 1 entfernt

--- testfiles/testfiles_json/schliessanlage.py
+++ testfiles/testfiles_json/schliessanlage.py
@@ -13,6 +13,7 @@ GPIO.setup(40, GPIO.IN)  # Grüner Taser(Löschen)
 
 reader = SimpleMFRC522()
 
+
 while True:
     if 1 == 0:  # GPIO.input(38) == 0 or GPIO.input(40) == 0:
         time.sleep(30)  # Hier fehlt noch speichern und löschen
