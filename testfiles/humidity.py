ea303c0f47fc794c608069ec88f89a5e3cffea2a
trimixtimo <timok95@gmail.com>
2021-12-15 11:45:34 +0100

    .

--- testfiles/humidity.py
+++ testfiles/humidity.py
@@ -140,7 +140,7 @@ def _main():
         temperatur = messwerte[13:18]
         luftfeuchte = messwerte[33:38]
         voc = messwerte[49:52]
-        voc = voc.replace("//","")
+        voc = voc.replace('"',"")
         
         with open ("/home/pi/config_dateien/universe.json") as json_file:
             x = json.load(json_file)
