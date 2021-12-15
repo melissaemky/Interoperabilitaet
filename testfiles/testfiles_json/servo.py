f52036392aeae4958c21d385cb8f89c5ed2c7a34
trimixtimo <timok95@gmail.com>
2021-12-15 14:10:34 +0100

    servo um intioalisierunsfunktion erweitert

--- testfiles/testfiles_json/servo.py
+++ testfiles/testfiles_json/servo.py
@@ -1,30 +1,31 @@
 import time
-
 import RPi.GPIO as GPIO
-servoPIN = 12
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servoPIN, GPIO.OUT)
-GPIO.setwarnings(False)
-p = GPIO.PWM(servoPIN, 50)  # GPIO 12 als PWM mit 50Hz
-p.start(10)  # Initialisierung
 
+def türini():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.OUT)
+    GPIO.setwarnings(False)
+    p = GPIO.PWM(12, 50)  # GPIO 12 als PWM mit 50Hz
+    p.start(10)  # Initialisierung
 
 def tuerzu():
+    p = GPIO.PWM(12, 50)  # GPIO 12 als PWM mit 50Hz
     p.ChangeDutyCycle(10)
     print("Tür ist geschlossen!")
     time.sleep(2)
-
+    GPIO.cleanup()
 
 def tuerauf():
+    p = GPIO.PWM(12, 50)  # GPIO 12 als PWM mit 50Hz
     p.ChangeDutyCycle(3.5)
     print("Tür ist geöffnet!")
     time.sleep(2)
+    GPIO.cleanup()
 
 
 def test(winkel):
-    if uni == 1:
-        p.ChangeDutyCycle(winkel)
-        time.sleep(2)
+    p.ChangeDutyCycle(winkel)
+    time.sleep(2)
     print(winkel)
 
-    # GPIO.cleanup()
+
