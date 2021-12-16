1740041b0257b252693d328a0f2f904225407a73
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-12-16 16:27:13 +0100

    zurückschreiben in  universtest

--- testfiles/testfiles_json/rfid_programing.py
+++ testfiles/testfiles_json/rfid_programing.py
@@ -36,8 +36,9 @@ def speichern():
             for j in range(0, lb):
                 benutzerkarte = (x['benutzer'][j]['kartenid'])
                 if kartenid == benutzerkarte:
-                    zugang = (x['benutzer'][j]['zugang'])
-                    zugang == "ja"
+                    (x['benutzer'][j]['zugang']) = "ja"
+                    with open('/home/pi/config_dateien/universetest.json', 'w') as json_file:
+                        json.dump(x, json_file, indent=4)
                     print("Zugang auf 'ja' gesetzt")  # kann später weg
         '''
         else:
@@ -63,18 +64,18 @@ def löschen():
         if kartennummer == str(id):
             print("if abfrage ist wahr")  # kann später weg
             kartenid = (x['karten'][i]['id'])
-            print("Kartennummer gefunden")  # kann später weg
+            print("Kartennummer gefunden" + kartenid)  # kann später weg
             for j in range(0, lb):
                 benutzerkarte = (x['benutzer'][j]['kartenid'])
                 if kartenid == benutzerkarte:
-                    zugang = (x['benutzer'][j]['zugang'])
-                    zugang == "nein"
+                    (x['benutzer'][j]['zugang']) = "nein"
+                    with open('/home/pi/config_dateien/universetest.json', 'w') as json_file:
+                        json.dump(x, json_file, indent=4)
                     print("Zugang auf 'nein' gesetzt")  # kann später weg
 
 
 with open("/home/pi/config_dateien/taster.json") as json_file:
     x = json.load(json_file)
-    print(type(x))
     lt = len(x['taster'])
     for k in range(0, lt):
         taster = (x['taster'][k]['name'])
