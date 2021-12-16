3f8e09101ef284d4d66668f06d0b4f6c6a639e79
Timo <timok95@gmail.com>
2021-12-16 16:02:50 +0100

    lastseen in json abgefragt

--- testfiles/testfiles_json/bewegungssensor_zigbee.py
+++ testfiles/testfiles_json/bewegungssensor_zigbee.py
@@ -6,6 +6,6 @@ def lesen():
     pir = requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/00:50:43:c9:a3:39:4a:82-01-0500/')
     body = json.loads(pir.text)
     state = (body["state"]["presence"])
-    zeitpunkt = (body["state"]["presence"])
+    zeitpunkt = (body["lastseen"])
     print(state)
     print(zeitpunkt)
