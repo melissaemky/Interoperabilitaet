d9b47c892b9d2daa3abcc1fc2ecdac19831d96e7
alex060202 <al3x060202@gmail.com>
2021-12-16 10:20:45 +0100

    ..

---
+++ Bildverarbeitung/How we do it.m
@@ -0,0 +1,3 @@
+Restau_21_1.mat von Moodle herunterladen und im Matlab verzeichnis speichern
+Datei über load('Restau_21_1.mat') im Command Window öffnen
+Bild öffnen: imagesc (Bildname) => imagesc (A4)
