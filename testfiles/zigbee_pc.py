c99105ca4a03fbd51bfa0346359e0d0907700781
Timo <timok95@gmail.com>
2021-12-16 16:09:48 +0100

    .

--- testfiles/zigbee_pc.py
+++ testfiles/zigbee_pc.py
@@ -5,7 +5,7 @@ import time
 
 pir =requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/00:50:43:c9:a3:39:4a:82-01-0500/')
 body = json.loads(pir.text)
-state = (body["state"]["presence"])
+state = (body["lastseen"])
 print(state)
 '''
 zustand="true"
