d924f8a1d761059200e6e7ce579993bfcef917d3
Timo <timok95@gmail.com>
2021-12-16 16:28:37 +0100

    last state-updated statt lastseen

--- testfiles/testfiles_json/bewegungssensor_zigbee.py
+++ testfiles/testfiles_json/bewegungssensor_zigbee.py
@@ -6,7 +6,7 @@ def lesen():
     pir = requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/00:50:43:c9:a3:39:4a:82-01-0500/')
     body = json.loads(pir.text)
     state = (body["state"]["presence"])
-    zeitpunkt = (body["lastseen"])
+    zeitpunkt = (body["state"]["lastupdated"])
     print(state)
     print(zeitpunkt)
     with open ("/home/pi/config_dateien/universe.json") as json_file:
