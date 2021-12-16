76e18210c57635e65e2a5305c56c39d94de30e5a
alex060202 <al3x060202@gmail.com>
2021-12-16 10:22:30 +0100

    ..

--- Bildverarbeitung/How we do it.m
+++ Bildverarbeitung/How we do it.m
@@ -1,3 +1,3 @@
-Restau_21_1.mat von Moodle herunterladen und im Matlab verzeichnis speichern
+(Restau_21_1.mat von Moodle herunterladen und im Matlab verzeichnis speichern) Kann weggelassen werden weil im GIT
 Datei über load('Restau_21_1.mat') im Command Window öffnen
 Bild öffnen: imagesc (Bildname) => imagesc (A4)
