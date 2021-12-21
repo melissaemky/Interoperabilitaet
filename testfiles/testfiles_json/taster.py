a8aa166726d54c223011b9a10582662669ae1ea2
Timo <timok95@gmail.com>
2021-12-21 11:39:18 +0100

    tasterabfrage in while schleife

--- testfiles/testfiles_json/taster.py
+++ testfiles/testfiles_json/taster.py
@@ -18,30 +18,26 @@ def time_unix(timetoconvert):
     unix = int(time.mktime(t))
     return unix
 
-'''
-now = time.time()
-now = int(now)
-print(now)
-'''
+while(1):
 
-tasterB1 =requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/7/')
-body1 = json.loads(tasterB1.text)
-state1 = (body1["state"]["buttonevent"])
-xtime1 = (body1["state"]["lastupdated"])
+    tasterB1 =requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/7/')
+    body1 = json.loads(tasterB1.text)
+    state1 = (body1["state"]["buttonevent"])
+    xtime1 = (body1["state"]["lastupdated"])
 
-tasterB2 =requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/8/')
-body2 = json.loads(tasterB2.text)
-state2 = (body2["state"]["buttonevent"])
-xtime2 = (body2["state"]["lastupdated"])
+    tasterB2 =requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/8/')
+    body2 = json.loads(tasterB2.text)
+    state2 = (body2["state"]["buttonevent"])
+    xtime2 = (body2["state"]["lastupdated"])
 
-with open ("/home/pi/config_dateien/taster.json") as json_file:
-    x = json.load(json_file)
-    x["taster"][4]["zustand"]= state1
-    x["taster"][4]["zeitpunkt"]= time_unix(xtime1)
-    x["taster"][5]["zustand"]= state2
-    x["taster"][5]["zeitpunkt"]= time_unix(xtime2)
-    print(x)
+    with open ("/home/pi/config_dateien/taster.json") as json_file:
+        x = json.load(json_file)
+        x["taster"][4]["zustand"]= state1
+        x["taster"][4]["zeitpunkt"]= time_unix(xtime1)
+        x["taster"][5]["zustand"]= state2
+        x["taster"][5]["zeitpunkt"]= time_unix(xtime2)
+        print(x)
 
 
-with open("/home/pi/config_dateien/taster.json", 'w') as json_file:
-    json.dump(x, json_file, indent=4)
+    with open("/home/pi/config_dateien/taster.json", 'w') as json_file:
+        json.dump(x, json_file, indent=4)
