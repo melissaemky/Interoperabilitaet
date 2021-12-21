6af284fca9ed4e0f275ec2758af6a5beb18e9238
Timo <timok95@gmail.com>
2021-12-21 10:07:52 +0100

    .

--- trash/unixtest.py
+++ trash/unixtest.py
@@ -2,12 +2,12 @@ import time
 import json
 
 now = time.time()
-print(now)
 now = int(now)
 print(now)
 
 with open ("/home/pi/config_dateien/taster.json") as json_file:
     x = json.load(json_file)
+    print(x["taster"][0]["name"])
     x["taster"][0]["zeitpunkt"]=now
 
 with open("/home/pi/config_dateien/taster.json", 'w') as json_file:
