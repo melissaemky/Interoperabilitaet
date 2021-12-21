4491bfd399e6e2e83688955b421fdd1dbf9b0e09
Timo <timok95@gmail.com>
2021-12-21 10:05:06 +0100

    pfad berichtigt

--- trash/unixtest.py
+++ trash/unixtest.py
@@ -10,7 +10,7 @@ with open ("/home/pi/config_dateien/taster.json") as json_file:
     x = json.load(json_file)
     x["taster"][0]["zeitpunkt"]=now
 
-with open("test.json", 'w') as json_file:
+with open("/home/pi/config_dateien/taster.json", 'w') as json_file:
     json.dump(x, json_file, indent=4)
 
 
