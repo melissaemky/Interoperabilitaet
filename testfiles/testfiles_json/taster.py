19e8009e3686aecc0663fcb62380c782f93edf1f
melissaemky <81586491+melissaemky@users.noreply.github.com>
2021-12-22 14:59:45 +0100

    taster.py zum anfang zurückgesetzt

--- testfiles/testfiles_json/taster.py
+++ testfiles/testfiles_json/taster.py
@@ -16,9 +16,10 @@ def time_unix(timetoconvert):
 
 
 while(1):
+    '''
     with open("/home/pi/config_dateien/taster.json") as json_file:
         x = json.load(json_file)
-
+    '''
     tasterB1 = requests.get('http://192.168.8.215/api/C0CEFA0EB7/sensors/7/')
     body1 = json.loads(tasterB1.text)
     state1 = str(body1["state"]["buttonevent"])
@@ -29,6 +30,7 @@ while(1):
     state2 = str(body2["state"]["buttonevent"])
     xtime2 = (body2["state"]["lastupdated"])
 
+    '''
     if GPIO.input(38) == 0:
         blau = 1
         zustand = x["taster"][1]["zustand"]
@@ -50,16 +52,25 @@ while(1):
                 break
             else:
                 blau = 0
+    '''
+    if GPIO.input(38) == 0:
+        blau = 1
+    else:
+        blau = 0
+
     if GPIO.input(40) == 0:
         gruen = 1
     else:
         gruen = 0
 
-    x["taster"][4]["zustand"] = state1[0]
-    x["taster"][4]["zeitpunkt"] = time_unix(xtime1)
-    x["taster"][5]["zustand"] = state2[0]
-    x["taster"][5]["zeitpunkt"] = time_unix(xtime2)
-    x["taster"][3]["zustand"] = str(gruen)
+    with open("/home/pi/config_dateien/taster.json") as json_file:
+        x = json.load(json_file)
+        x["taster"][4]["zustand"] = state1[0]
+        x["taster"][4]["zeitpunkt"] = time_unix(xtime1)
+        x["taster"][5]["zustand"] = state2[0]
+        x["taster"][5]["zeitpunkt"] = time_unix(xtime2)
+        x["taster"][1]["zustand"] = str(blau)
+        x["taster"][3]["zustand"] = str(gruen)
 
     with open("/home/pi/config_dateien/taster.json", 'w') as json_file:
         json.dump(x, json_file, indent=4)
