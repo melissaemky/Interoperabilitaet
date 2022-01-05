fac9b0dee9543253880b5370c399009824cf5580
melissaemky <81586491+melissaemky@users.noreply.github.com>
2022-01-05 15:13:00 +0100

    ...

--- testfiles/testfiles_json/schliessanlage.py
+++ testfiles/testfiles_json/schliessanlage.py
@@ -35,39 +35,41 @@ def schliessanlage():
             print("meep")
             id = reader.read()
 
-        with open("/home/pi/config_dateien/universe.json") as json_file:
-            x = json.load(json_file)
-        lk = len(x['karten'])  # Anzahl der gespeicherten Karten
-        lb = len(x['benutzer'])  # Anzahl der gespeicherten Benutzer
-        la = len(x['aktoren'])  # Anzahl der gespeicherten Aktoren
-        for i in range(0, lk):  # Gespeicherte Karten werden durchlaufen
-            kartennummer = (x['karten'][i]['kartennummer'])
-            if kartennummer == id:  # Karte wurde schonmal gespeichert
-                kartenid = (x['karten'][i]['id'])
-                for j in range(0, lb):  # Benutzer dieser Karte finden
-                    benutzerkarte = (x['benutzer'][j]['kartenid'])
-                    if kartenid == benutzerkarte:  # Benutzer gefunden
-                        zugang = (x['benutzer'][j]['zugang'])
-                        if zugang == "ja":  # Benutzer hat Zugang
-                            for k in range(0, la):  # Haustür finden
-                                aktor = (x['aktoren'][k]['typ'])
-                                if aktor == "haustuer":  # Haustür gefunden
-                                    zustand = (x['aktoren'][k]['zustand'])
-                                    # Wenn Haustür geschlossen ist, dann öffnen:
-                                    if zustand == "0":
-                                        tuerini()
-                                        tuerauf()
-                                        # Zustand auf "Geöffnet"
-                                        (x['aktoren'][k]['zustand']) = "1"
-                                        # Neuen Zustand speichern:
-                                        with open('/home/pi/config_dateien/universe.json', 'w') as json_file:
-                                            json.dump(x, json_file, indent=4)
-                                    else:
-                                        # Wenn Haustür geöffnet ist, dann schließen:
-                                        tuerini()
-                                        tuerzu()
-                                        # Zustand auf "Geschlossen"
-                                        (x['aktoren'][k]['zustand']) = "0"
-                                        # Neuen Zustand speichern:
-                                        with open('/home/pi/config_dateien/universe.json', 'w') as json_file:
-                                            json.dump(x, json_file, indent=4)
+            with open("/home/pi/config_dateien/universe.json") as json_file:
+                x = json.load(json_file)
+            lk = len(x['karten'])  # Anzahl der gespeicherten Karten
+            lb = len(x['benutzer'])  # Anzahl der gespeicherten Benutzer
+            la = len(x['aktoren'])  # Anzahl der gespeicherten Aktoren
+            for i in range(0, lk):  # Gespeicherte Karten werden durchlaufen
+                kartennummer = (x['karten'][i]['kartennummer'])
+                if kartennummer == id:  # Karte wurde schonmal gespeichert
+                    kartenid = (x['karten'][i]['id'])
+                    for j in range(0, lb):  # Benutzer dieser Karte finden
+                        benutzerkarte = (x['benutzer'][j]['kartenid'])
+                        if kartenid == benutzerkarte:  # Benutzer gefunden
+                            zugang = (x['benutzer'][j]['zugang'])
+                            if zugang == "ja":  # Benutzer hat Zugang
+                                for k in range(0, la):  # Haustür finden
+                                    aktor = (x['aktoren'][k]['typ'])
+                                    if aktor == "haustuer":  # Haustür gefunden
+                                        zustand = (x['aktoren'][k]['zustand'])
+                                        # Wenn Haustür geschlossen ist, dann öffnen:
+                                        if zustand == "0":
+                                            tuerini()
+                                            tuerauf()
+                                            # Zustand auf "Geöffnet"
+                                            (x['aktoren'][k]['zustand']) = "1"
+                                            # Neuen Zustand speichern:
+                                            with open('/home/pi/config_dateien/universe.json', 'w') as json_file:
+                                                json.dump(
+                                                    x, json_file, indent=4)
+                                        else:
+                                            # Wenn Haustür geöffnet ist, dann schließen:
+                                            tuerini()
+                                            tuerzu()
+                                            # Zustand auf "Geschlossen"
+                                            (x['aktoren'][k]['zustand']) = "0"
+                                            # Neuen Zustand speichern:
+                                            with open('/home/pi/config_dateien/universe.json', 'w') as json_file:
+                                                json.dump(
+                                                    x, json_file, indent=4)
