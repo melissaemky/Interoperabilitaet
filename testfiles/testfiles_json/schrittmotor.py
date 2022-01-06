b94d7fabe56eec182c60c6b228b90684dabe0c8d
melissaemky <81586491+melissaemky@users.noreply.github.com>
2022-01-06 10:54:52 +0100

    .

--- testfiles/testfiles_json/schrittmotor.py
+++ testfiles/testfiles_json/schrittmotor.py
@@ -58,5 +58,5 @@ if __name__ == "__main__":
     while True:
         steps = 100  # ("Wie viele Schritte vorwaerts? ")
         forward(int(delay) / 1000.0, int(steps))
-        //steps = 10  # ("Wie viele Schritte rueckwaerts? ")
-        //backwards(int(delay) / 1000.0, int(steps))
+        # steps = 10  # ("Wie viele Schritte rueckwaerts? ")
+        # backwards(int(delay) / 1000.0, int(steps))
