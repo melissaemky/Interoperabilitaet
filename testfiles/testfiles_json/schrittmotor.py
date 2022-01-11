1d7e8e5a4cc00e18170cc1b6b85a6c181a38a82a
melissaemky <81586491+melissaemky@users.noreply.github.com>
2022-01-11 11:02:49 +0100

    Code aufgeräumt

--- testfiles/testfiles_json/schrittmotor.py
+++ testfiles/testfiles_json/schrittmotor.py
@@ -4,11 +4,10 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-coil_A_1_pin = 35  # blau
-coil_A_2_pin = 31  # gelb
-coil_B_1_pin = 33  # pink
-coil_B_2_pin = 29  # orange
-# enable_pin   = 7 # Nur bei bestimmten Motoren benoetigt (+Zeile 24 und 30)
+coil_A_1_pin = 35  # blaues kabel
+coil_A_2_pin = 31  # gelbes kabel
+coil_B_1_pin = 33  # pinkes kabel
+coil_B_2_pin = 29  # oranges kabel
 
 # anpassen, falls andere Sequenz
 StepCount = 8
@@ -22,15 +21,12 @@ Seq[5] = [1, 0, 1, 0]
 Seq[6] = [0, 0, 1, 0]
 Seq[7] = [0, 1, 1, 0]
 
-# GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
 GPIO.setup(8, GPIO.IN)
 GPIO.setup(10, GPIO.IN)
-
-# GPIO.output(enable_pin, 1)
 
 
 def setStep(w1, w2, w3, w4):
@@ -40,15 +36,15 @@ def setStep(w1, w2, w3, w4):
     GPIO.output(coil_B_2_pin, w4)
 
 
-def forward(delay, steps):
+def forward(delay, steps):  # Türe schließen
     for i in range(steps):
         for j in range(StepCount):
             setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
             time.sleep(delay / 1000)
 
 
-def backwards(delay, steps):
-    while GPIO.input(10) == 0:
+def backwards(delay, steps):  # Türe öffnen
+    while GPIO.input(10) == 0:  # Stoppt bei passieren der Lichtschranke
         print(GPIO.input(10))
         for i in range(steps):
             for j in reversed(range(StepCount)):
@@ -56,11 +52,11 @@ def backwards(delay, steps):
                 time.sleep(delay / 1000)
 
 
+# Zum testen, nicht in Funktion genutzt:
 if __name__ == "__main__":
     while True:
-        steps = 200  # ("Wie viele Schritte vorwaerts? ")
+        steps = 200  # Wie viele steps?
         forward(1, int(steps))
         time.sleep(5)
-        # steps = 10  # ("Wie viele Schritte rueckwaerts? ")
         backwards(1, int(steps))
         time.sleep(5)
