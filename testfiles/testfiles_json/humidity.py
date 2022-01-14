55914a6a53f2f8aecb2c5d997f4b16028a99ec4e
Timo <timok95@gmail.com>
2022-01-14 15:40:44 +0100

    ...

--- testfiles/testfiles_json/humidity.py
+++ testfiles/testfiles_json/humidity.py
@@ -134,7 +134,7 @@ def luftsensor():
     luftfeuchte = luftfeuchte.replace('"',"")
     temperatur = temperatur.replace('"',"")
 
-    print(voc + "\\n" + luftfeuchte + "\\n" + temperatur)
+    print(voc + " " + luftfeuchte + " " + temperatur)
 """
 if __name__ == "__main__":
     _main()
